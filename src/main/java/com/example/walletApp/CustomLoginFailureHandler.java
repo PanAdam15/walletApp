cdaf94c44c8287e97dcf409500bde7f751e94344
adamx <adamxpankowski@gmail.com>
2023-01-08 17:42:00 +0100

    Cleaning

--- src/main/java/com/example/walletApp/CustomLoginFailureHandler.java
+++ src/main/java/com/example/walletApp/CustomLoginFailureHandler.java
@@ -1,7 +1,6 @@
 package com.example.walletApp;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
@@ -42,35 +41,27 @@ public class CustomLoginFailureHandler extends SimpleUrlAuthenticationFailureHan
 
             loginResultRepository.save(loginResult);
             userService.increaseFailedAttempts(user);
-            System.out.println("KUTASOW W DUPIE SZUBIGI:" + user.getFailedAttempt());
 
 
-            if(user.isEnabled() && user.isAccountNonLocked()){
-            if (user.getFailedAttempt() < CustomUserDetailsService.TWO_FAILED - 1) {
-                super.setDefaultFailureUrl("/login?error");
-                System.out.println("KUTASOW W DUPIE SZUBIGI:" + user.getFailedAttempt());
-            } else if (user.getFailedAttempt() == CustomUserDetailsService.TWO_FAILED - 1) {
-                super.setDefaultFailureUrl("/login?error2");
-                userService.lock(user);
-                userService.lockedAccountFor(user,CustomUserDetailsService.FIVE_SECONDS_LOCK);
-                System.out.println("KUTASOW W DUPIE SZUBIGI:" + user.getFailedAttempt());
-//                    exception = new LockedException("Your account has been locked due to 2 failed attempts."
-//                            + " It will be unlocked after 5 seconds.");
-            } else if (user.getFailedAttempt() == CustomUserDetailsService.THREE_FAILED - 1) {
-                super.setDefaultFailureUrl("/login?error3");
-                userService.lock(user);
-                userService.lockedAccountFor(user, CustomUserDetailsService.TEN_SECONDS_LOCK);
-                System.out.println("KUTASOW W DUPIE SZUBIGI:" + user.getFailedAttempt());
-//                    exception = new LockedException("Your account has been locked due to 2 failed attempts."
-//                            + " It will be unlocked after 10 seconds.");
-            } else if (user.getFailedAttempt() >= CustomUserDetailsService.FOUR_FAILED - 1) {
-                super.setDefaultFailureUrl("/login?error4");
-                userService.lock(user);
-                userService.lockedAccountFor(user,CustomUserDetailsService.TWO_MINUTES_LOCK);
-                System.out.println("KUTASOW W DUPIE SZUBIGI:" + user.getFailedAttempt());
+            if (user.isEnabled() && user.isAccountNonLocked()) {
+                if (user.getFailedAttempt() < CustomUserDetailsService.TWO_FAILED - 1) {
+                    super.setDefaultFailureUrl("/login?error");
+                } else if (user.getFailedAttempt() == CustomUserDetailsService.TWO_FAILED - 1) {
+                    super.setDefaultFailureUrl("/login?error2");
+                    userService.lock(user);
+                    userService.lockedAccountFor(user, CustomUserDetailsService.FIVE_SECONDS_LOCK);
+                } else if (user.getFailedAttempt() == CustomUserDetailsService.THREE_FAILED - 1) {
+                    super.setDefaultFailureUrl("/login?error3");
+                    userService.lock(user);
+                    userService.lockedAccountFor(user, CustomUserDetailsService.TEN_SECONDS_LOCK);
 
-            }
-            }else if (!user.isAccountNonLocked()) {
+                } else if (user.getFailedAttempt() >= CustomUserDetailsService.FOUR_FAILED - 1) {
+                    super.setDefaultFailureUrl("/login?error4");
+                    userService.lock(user);
+                    userService.lockedAccountFor(user, CustomUserDetailsService.TWO_MINUTES_LOCK);
+
+                }
+            } else if (!user.isAccountNonLocked()) {
                 if (userService.unlockWhenTimeExpired(user)) {
                     super.setDefaultFailureUrl("/login?errorUn");
                 }
